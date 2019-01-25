900d43da204981932babb9eeca876acc0ab64c6a
Dylan Harries <Dylan.Harries@csiro.au>
2019-01-25 17:15:06 +1100

    Generate additional longer series for Lorenz-96 model

--- examples/lorenz96/generate_lorenz96_test_data.sh
+++ examples/lorenz96/generate_lorenz96_test_data.sh
@@ -9,8 +9,7 @@ data_dir="input-data"
 
 forcing_types="constant periodic"
 forcing_amps="3 5.3 7 10"
-lengths="100 500 1000 3000 10000"
-#"100 500 1000 3000 10000 50000 100000 1000000"
+lengths="100 500 1000 3000 10000 50000 100000 1000000"
 time_step="0.01"
 
 if test ! -d "$data_dir" ; then
@@ -19,9 +18,10 @@ fi
 
 for t in $forcing_types ; do
     prefix="${model}_${t}_forcing"
+    suffix="_time_step-${time_step}.csv"
     for a in $forcing_amps ; do
         for l in $lengths ; do
-            output_file="${data_dir}/${prefix}_F-${a}_length-${l}_time_step-${time_step}.csv"
+            output_file="${data_dir}/${prefix}_F-${a}_length-${l}${suffix}"
             $PYTHON $run_cmd \
                     --forcing-type $t \
                     --forcing-amp $a \
