70e41030944236653634445873772dfca2b661a6
Dylan Harries <Dylan.Harries@csiro.au>
2019-02-01 15:37:15 +1100

    Separate sorting and determining projection into two functions

--- pyspa/constraints/simplex_projection.py
+++ pyspa/constraints/simplex_projection.py
@@ -1,34 +1,38 @@
 import numpy as np
 
-def simplex_projection(data):
-    shape = data.shape
-    if np.size(shape) == 1:
-        T = 1
-        n = shape[0]
-        data_sorted = np.sort(np.reshape(data, (1, n)))
-    elif np.size(shape) == 2:
-        (T, n) = shape
-        data_sorted = np.sort(data, axis=1)
-    else:
-        raise ValueError("input data must be a vector or matrix")
-
+def simplex_projection_presorted(sorted_data):
+    (T, n) = sorted_data.shape
     t_hat = np.zeros(T)
     i = n - 2
     idxs = i * np.ones(T)
     num_to_resolve = T
     while num_to_resolve > 0 and i >= 0:
-        t_hat[idxs == i] = ((np.sum(data_sorted[idxs == i, i+1:], axis=1) - 1)
+        t_hat[idxs == i] = ((np.sum(sorted_data[idxs == i, i+1:], axis=1) - 1)
                             / (n - i - 1))
-        idxs[(t_hat >= data_sorted[:, i]) & (idxs == i)] = -2
+        idxs[(t_hat >= sorted_data[:, i]) & (idxs == i)] = -2
         idxs[idxs == i] = i - 1
         i -= 1
         num_to_resolve = np.sum(idxs == i)
 
-    t_hat[idxs == -1] = (np.sum(data_sorted[idxs == -1, :], axis=1) - 1) / n
+    t_hat[idxs == -1] = (np.sum(sorted_data[idxs == -1, :], axis=1) - 1) / n
 
-    projections = np.fmax(data - np.reshape(t_hat, (T, 1)), 0)
+    return np.fmax(sorted_data - np.reshape(t_hat, (T, 1)), 0)
 
-    if np.size(shape) == 1:
+def simplex_projection(data):
+    ndims = data.ndim
+    if ndims == 1:
+        T = 1
+        n = data.shape[0]
+        sorted_data = np.sort(np.reshape(data, (1, n)))
+    elif ndims == 2:
+        (T, n) = data.shape
+        sorted_data = np.sort(data, axis=1)
+    else:
+        raise ValueError("input data must be a vector or matrix")
+
+    projections = simplex_projection_presorted(sorted_data)
+
+    if ndims == 1:
         return np.ravel(projections)
     else:
         return projections
