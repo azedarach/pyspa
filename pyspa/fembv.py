207b3f1a0225b0f043fef4ccab2be2ced270c883
Dylan Harries <Dylan.Harries@csiro.au>
2019-04-12 18:10:32 +1000

    Add initial (buggy) implementation of causal FEM-BV algorithm

--- pyspa/fembv.py
+++ pyspa/fembv.py
@@ -6,7 +6,8 @@ import os
 import pickle
 import scipy.sparse as sps
 
-from scipy.optimize import linprog
+from scipy.optimize import (Bounds, LinearConstraint, linprog, minimize)
+from scipy.spatial import ConvexHull
 
 from sklearn.cluster import KMeans
 from sklearn.utils import check_array, check_random_state
@@ -688,6 +689,337 @@ def fembv_kmeans(X, Gamma=None, Theta=None, n_components=None,
             _fembv_kmeans_Theta_update,
             theta_update_pars=None, distance_matrix_pars=None,
             epsilon_Theta=0, regularization_Theta=None,
+            tol=tol, max_iter=max_iter, update_Theta=update_Theta,
+            max_tv_norm=max_tv_norm, fem_basis=fem_basis,
+            method=method,
+            verbose=verbose, checkpoint=checkpoint,
+            checkpoint_file=checkpoint_file,
+            checkpoint_iter=checkpoint_iter)
+    else:
+        raise ValueError("Invalid solver parameter '%s'." % solver)
+
+    if n_iter == max_iter and tol > 0:
+        warnings.warn('Maximum number of iterations %d reached.' % max_iter,
+                      warnings.UserWarning)
+
+    return Gamma, Theta, n_iter
+
+
+def _fembv_binx_lambda_vector(i, Theta, u=None):
+    if u is None:
+        return Theta[i, :]
+
+    if u.ndim == 1:
+        n_samples = 1
+        n_external = u.shape[0]
+        u_mat = np.reshape(u, (n_samples, n_external))
+    else:
+        n_samples, n_external = u.shape
+        u_mat = u
+
+    n_features = int(Theta.shape[1] / (n_external + 1))
+
+    lam = np.broadcast_to(
+        Theta[i, :n_features], (n_samples, n_features)).copy()
+
+    for k in range(n_external):
+        lk = Theta[i, (k + 1) * n_features:(k + 2) * n_features]
+        lam += lk[np.newaxis, :] * np.broadcast_to(
+            np.reshape(u_mat[:, k], (n_samples, 1)), (n_samples, n_features))
+
+    if n_samples == 1:
+        return np.ravel(lam)
+    else:
+        return lam
+
+
+def _fembv_binx_distance_matrix(YX, Theta, u=None):
+    Y = YX[:, 0]
+    X = YX[:, 1:]
+
+    n_samples, n_features = X.shape
+    n_components = Theta.shape[0]
+
+    G = np.zeros((n_samples, n_components))
+    for i in range(n_samples):
+        for j in range(n_components):
+            if u is None:
+                lam = _fembv_binx_lambda_vector(j, Theta)
+            else:
+                lam = _fembv_binx_lambda_vector(j, Theta, u=u[i, :])
+            lxp = np.dot(lam, X[i, :])
+            G[i, j] = -((1 - Y[i]) * np.log(1 - lxp) + Y[i] * np.log(lxp))
+
+    return G
+
+
+def _fembv_binx_Theta_regularization(Theta, u=None, epsilon_Theta=0, *pars):
+    if u is None:
+        return np.sum(Theta)
+    else:
+        return np.sum(Theta ** 2)
+
+
+def _fembv_binx_distance_grad(YX, Gamma, Theta, u=None):
+    Y = YX[:, 0]
+    X = YX[:, 1:]
+
+    n_samples, n_features = X.shape
+    n_components = Theta.shape[0]
+
+    if u is None:
+        n_external = 0
+    else:
+        n_external = u.shape[1]
+
+    n_pars = n_features * (n_external + 1)
+
+    yp = Y[:, np.newaxis] * X
+    yp_comp = (1 - Y[:, np.newaxis]) * X
+    DG = np.zeros((n_components, n_pars))
+    for i in range(n_components):
+        gyp = Gamma[:, i][:, np.newaxis] * yp
+        gyp_comp = Gamma[:, i][:, np.newaxis] * yp_comp
+        if u is None:
+            ldotp = np.sum(Theta[i, :][np.newaxis, :] * X, axis=1)
+            for j in range(n_pars):
+                DG[i, j] = -np.sum(
+                    np.divide(gyp[:, j], ldotp) -
+                    np.divide(gyp_comp[:, j], 1 - ldotp))
+        else:
+            lx = _fembv_binx_lambda_vector(i, Theta, u=u)
+            lxdotp = np.sum(lx * X, axis=1)
+            for j in range(n_features):
+                DG[i, j] = -np.sum(
+                    np.divide(gyp[:, j], lxdotp) -
+                    np.divide(gyp_comp[:, j], 1 - lxdotp))
+            for k in range(n_external):
+                for j in range(n_features):
+                    par_idx = j + (k + 1) * n_features
+                    DG[i, par_idx] = np.sum(
+                        np.divide(gyp[:, j] * u[:, k], lxdotp) -
+                        np.divide(gyp_comp[:, j] * u[:, k], 1 - lxdotp))
+
+    return DG
+
+
+def _fembv_binx_regularization_grad(Theta, u=None, epsilon_Theta=0):
+    return epsilon_Theta * np.ones(Theta.shape)
+
+
+def _fembv_binx_cost_grad(YX, Gamma, Theta, u=None, epsilon_Theta=0):
+    return (_fembv_binx_distance_grad(YX, Gamma, Theta, u=u) +
+            _fembv_binx_regularization_grad(
+                Theta, epsilon_Theta=epsilon_Theta))
+
+
+def _fembv_binx_cost_hess(YX, Theta, u=None):
+    Y = YX[:, 0]
+    X = YX[:, 1:]
+
+    n_samples, n_features = X.shape
+    n_components = Theta.shape[0]
+
+    if u is None:
+        n_external = 0
+    else:
+        n_external = u.shape[1]
+
+    n_component_pars = n_features * (n_external + 1)
+
+    yp = Y[:, np.newaxis] * X
+    yp_comp = (1 - Y[:, np.newaxis]) * X
+    H = np.zeros((n_pars, n_pars))
+
+
+def _fembv_binx_cost(YX, Gamma, Theta, u=None, epsilon_Theta=0):
+    return _fembv_generic_cost(
+        YX, Gamma, Theta, _fembv_binx_distance_matrix,
+        distance_matrix_pars=u, epsilon_Theta=epsilon_Theta,
+        regularization_Theta=_fembv_binx_Theta_regularization)
+
+
+def _fembv_binx_Theta_bounds(n_components, n_features, u=None):
+    if u is None:
+        return Bounds(0, 1)
+
+    n_external = u.shape[1]
+    n_component_pars = n_features * (n_external + 1)
+    n_pars = n_components * n_component_pars
+    lb = np.full(n_pars, -np.inf)
+    ub = np.full(n_pars, np.inf)
+    for i in range(n_components):
+        lb[i * n_component_pars:i * n_component_pars + n_features] = 0
+        ub[i * n_component_pars:i * n_component_pars + n_features] = 1
+    return Bounds(lb, ub)
+
+
+def _fembv_binx_Theta_constraints(n_components, n_features, u=None):
+    if u is None:
+        n_pars = n_components * n_features
+        A_constr = np.zeros((n_components, n_pars))
+        for i in range(n_components):
+            A_constr[i, i * n_features:(i + 1) * n_features] = 1
+        return LinearConstraint(A_constr, 0, 1)
+
+    n_external = u.shape[1]
+    n_component_pars = n_features * (n_external + 1)
+    n_pars = n_components * n_component_pars
+
+    convex_hull = ConvexHull(u)
+    convex_hull_vertices = u[convex_hull.vertices]
+    n_vertices = convex_hull_vertices.shape
+
+    n_elem_constraints = n_components * n_features * n_vertices
+    A_elem = np.zeros((n_elem_constraints, n_pars))
+    for k in range(n_vertices):
+        vertex = convex_hull_vertices[k]
+        for i in range(n_components):
+            base_offset = i * n_component_pars
+            ext_start = base_offset + n_features
+            ext_end = ext_start + n_external * n_features
+            for j in range(n_features):
+                idx = j + i * n_component_pars + k * n_pars
+                A_elem[idx, j + base_offset] = 1
+                A_elem[idx, j + ext_start:j + ext_end:n_features] = vertex
+
+    n_sum_constraints = n_components * (n_vertices + 1)
+    A_sum = np.zeros((n_sum_constraints, n_pars))
+
+    for i in range(n_components):
+        A_sum[i, i * n_component_pars:i * n_component_pars + n_features] = 1
+
+    for k in range(n_vertices):
+        vertex = convex_hull_vertices[k]
+        for i in range(n_components):
+            idx = i + (k + 1) * n_components
+            base_offset = i * n_component_pars
+            ext_start = base_offset + n_features
+            A_sum[idx, i * n_component_pars:
+                  i * n_component_pars + n_features] = 1
+            for j in range(n_external):
+                A_sum[idx, j * n_features + ext_start:
+                      ext_start + (j + 1) * n_features] = vertex[j]
+
+    A_bounds = np.vstack([A_elem, A_sum])
+
+    return LinearConstraint(A_bounds, 0, 1)
+
+
+def _fembv_binx_Theta_update(YX, Gamma, Theta, pars, **kwargs):
+    u = pars[0]
+    epsilon_Theta = pars[1]
+    bounds = pars[2]
+    constraints = pars[3]
+
+    n_components, n_pars = Theta.shape
+
+    x0 = np.ravel(Theta)
+
+    args = (YX, Gamma, u, epsilon_Theta)
+
+    def f(x, *args):
+        yx = args[0]
+        gamma = args[1]
+        u = args[2]
+        epsilon_Theta = args[3]
+        theta = np.reshape(x, (n_components, n_pars))
+        return _fembv_binx_cost(
+            yx, gamma, theta, u=u, epsilon_Theta=epsilon_Theta)
+
+    def jac(x, *args):
+        yx = args[0]
+        u = args[2]
+        epsilon_Theta = args[3]
+        theta = np.reshape(x, (n_components, n_pars))
+        jac_mat = _fembv_binx_cost_grad(
+            yx, theta, u=u, epsilon_Theta=epsilon_Theta)
+        return np.ravel(jac_mat)
+
+    def hess(x, *args):
+        yx = args[0]
+        u = args[2]
+        epsilon_Theta = args[3]
+        theta = np.reshape(x, (n_components, n_pars))
+        hess_mat = _fembv_binx_cost_hess(yx, theta, u=u)
+        return hess_mat
+
+    res = minimize(f, x0, args=args, jac=jac, hess=hess,
+                   bounds=bounds, constraints=constraints, **kwargs)
+
+    if not res['success']:
+        raise RuntimeError('minimization of FEM-BV-BINX cost function failed')
+
+    sol = res['x']
+
+    return np.reshape(sol, (n_components, n_pars))
+
+
+def fembv_binx(X, Y, Gamma=None, Theta=None, u=None, n_components=None,
+               init=None, update_Theta=True, epsilon_Theta=0, solver='subspace',
+               tol=1e-4, max_iter=200, random_state=None, max_tv_norm=None,
+               fem_basis='constant', method='interior-point', verbose=0,
+               checkpoint=False, checkpoint_file=None, checkpoint_iter=None):
+    n_samples, n_features = X.shape
+    if n_components is None:
+        n_components = n_features
+
+    if not isinstance(n_components, INTEGER_TYPES) or n_components <= 0:
+        raise ValueError('Number of components must be a positive integer;'
+                         ' got (n_components=%r)' % n_components)
+    if not isinstance(max_iter, INTEGER_TYPES) or max_iter <= 0:
+        raise ValueError('Maximum number of iterations must be a positive '
+                         'integer; got (max_iter=%r)' % max_iter)
+    if not isinstance(tol, numbers.Number) or tol < 0:
+        raise ValueError('Tolerance for stopping criteria must be '
+                         'positive; got (tol=%r)' % tol)
+
+    if checkpoint:
+        if checkpoint_file is None:
+            raise ValueError('Name of checkpoint file must be given if '
+                             'checkpointing is enabled.')
+
+        if (not isinstance(checkpoint_iter, INTEGER_TYPES) or
+            checkpoint_iter <= 0):
+            raise ValueError(
+                'Number of iterations to checkpoint after must be a '
+                'positive integer; got (checkpoint_iter=%r)' % checkpoint_iter)
+
+    invalid_y_vals = np.any(np.logical_or(Y != 0, Y != 1))
+    if invalid_y_vals:
+        raise ValueError(
+            'data matrix Y must be a binary variable (values 0 or 1)')
+
+    yx = np.hstack([np.reshape(Y, (n_samples, 1)), X], axis=-1)
+
+    theta_update_bounds = _fembv_binx_Theta_bounds(
+        n_components, n_features, u=u)
+    theta_update_constraints = _fembv_binx_Theta_constraints(
+        n_components, n_features, u=u)
+    theta_update_pars = (u, epsilon_Theta,
+                         theta_update_bounds, theta_update_constraints)
+
+    if init == 'custom' and update_Theta:
+        _check_init_fembv_Gamma(Gamma, (n_samples, n_components),
+                                'FEM-BV-BINX (input Gamma)')
+        _check_init_fembv_binx_Theta(
+            Theta, (n_components, n_features), 'FEM-BV-BINX (input Theta)')
+    elif not update_Theta:
+        _check_init_fembv_binx_Theta(Theta, (n_components, n_features),
+                                     'FEM-BV-BINX (input Theta)')
+        Gamma, _ = _initialize_fembv_binx(yx, n_components, init=init,
+                                          random_state=random_state)
+    else:
+        Gamma, Theta = _initialize_fembv_binx(yx, n_components, init=init,
+                                              random_state=random_state)
+
+    if solver == 'subspace':
+        Gamma, Theta, n_iter = _fit_generic_fembv_subspace(
+            yx, Gamma, Theta, _fembv_binx_distance_matrix,
+            _fembv_binx_Theta_update,
+            theta_update_pars=theta_update_pars, distance_matrix_pars=u,
+            epsilon_Theta=epsilon_Theta,
+            regularization_Theta=_fembv_binx_Theta_regularization,
             tol=tol, max_iter=max_iter, update_Theta=update_Theta,
             max_tv_norm=max_tv_norm, fem_basis=fem_basis,
             method=method,
--- tests/test_fembv.py
+++ tests/test_fembv.py
@@ -3,7 +3,8 @@ import unittest
 import numpy as np
 
 from pyspa.fembv import (_fembv_Gamma_equality_constraints,
-                         _fembv_Gamma_upper_bound_constraints)
+                         _fembv_Gamma_upper_bound_constraints,
+                         _fembv_binx_cost, _fembv_binx_cost_grad)
 
 
 def _piecewise_constant_fem_basis(n_samples, n_elem=None, value=1):
@@ -27,6 +28,13 @@ def _random_fem_coefficients(n_elem, n_components):
     return np.random.uniform(low=0.0, high=1.0, size=(n_elem, n_components))
 
 
+def _random_affiliations(n_samples, n_components):
+    Gamma = np.random.uniform(low=0.0, high=1.0,
+                              size=(n_samples, n_components))
+    row_sums = np.sum(Gamma, axis=1)
+    return Gamma / row_sums[:, np.newaxis]
+
+
 class TestFEMBVGammaDiscretization(unittest.TestCase):
 
     def test_vector_form_constraints(self):
@@ -48,8 +56,8 @@ class TestFEMBVGammaDiscretization(unittest.TestCase):
         A_eq, b_eq = _fembv_Gamma_equality_constraints(n_components, V)
         A_ub, b_ub = _fembv_Gamma_upper_bound_constraints(n_components, V, c)
 
-        vec_eq_lhs = np.dot(A_eq, coeffs_vec)
-        vec_ub_lhs = np.dot(A_ub, coeffs_vec)
+        vec_eq_lhs = np.dot(A_eq.toarray(), coeffs_vec)
+        vec_ub_lhs = np.dot(A_ub.toarray(), coeffs_vec)
 
         expected_eq_lhs = np.sum(Gamma, axis=1)
 
@@ -59,7 +67,181 @@ class TestFEMBVGammaDiscretization(unittest.TestCase):
             [np.ravel(np.diff(Gamma, axis=0) - Eta),
              np.ravel(-np.diff(Gamma, axis=0) - Eta)])
         expected_ub_lhs = np.concatenate(
-            [expected_pos_lhs, expected_bv_lhs, expected_aux_lhs])
+            [expected_pos_lhs, expected_aux_lhs, expected_bv_lhs])
 
         self.assertTrue(np.allclose(vec_eq_lhs, expected_eq_lhs))
         self.assertTrue(np.allclose(vec_ub_lhs, expected_ub_lhs))
+
+
+def _central_derivative(f, x, args=(), h=1e-6):
+    return (f(x + h, *args) - f(x - h, *args)) / (2 * h)
+
+
+def _random_binary_sequence(n_samples):
+    seq = np.random.uniform(low=0.0, high=1.0, size=(n_samples,))
+    seq[seq < 0.5] = 0
+    seq[seq >= 0.5] = 1
+    return seq
+
+
+class TestFEMBVBINX(unittest.TestCase):
+
+    def test_cost_gradient_no_u_no_epsilon(self):
+        n_samples = 500
+        n_features = 3
+        n_components = 4
+
+        Y = _random_binary_sequence(n_samples)
+        X = _random_affiliations(n_samples, n_features)
+        YX = np.concatenate([np.reshape(Y, (n_samples, 1)), X], axis=-1)
+
+        Theta = _random_affiliations(n_components, n_features)
+        Gamma = _random_affiliations(n_samples, n_components)
+
+        def f(x, *args):
+            yx = args[0]
+            gamma = args[1]
+            theta = args[2].copy()
+            row_idx = args[3]
+            col_idx = args[4]
+
+            theta[row_idx, col_idx] = x
+
+            return _fembv_binx_cost(yx, gamma, theta)
+
+        numeric = np.zeros((n_components, n_features))
+        for i in range(n_components):
+            for j in range(n_features):
+                args = (YX, Gamma, Theta, i, j)
+                x = Theta[i, j]
+                numeric[i, j] = _central_derivative(f, x, args=args)
+
+        analytic = _fembv_binx_cost_grad(YX, Gamma, Theta)
+
+        self.assertTrue(np.allclose(numeric, analytic))
+
+    def test_cost_gradient_no_u_epsilon(self):
+        n_samples = 500
+        n_features = 3
+        n_components = 4
+
+        epsilon_Theta = np.random.uniform(low=2, high=10)
+
+        Y = _random_binary_sequence(n_samples)
+        X = _random_affiliations(n_samples, n_features)
+        YX = np.concatenate([np.reshape(Y, (n_samples, 1)), X], axis=-1)
+
+        Theta = _random_affiliations(n_components, n_features)
+        Gamma = _random_affiliations(n_samples, n_components)
+
+        def f(x, *args):
+            yx = args[0]
+            gamma = args[1]
+            theta = args[2].copy()
+            eps = args[3]
+            row_idx = args[4]
+            col_idx = args[5]
+
+            theta[row_idx, col_idx] = x
+
+            return _fembv_binx_cost(yx, gamma, theta, epsilon_Theta=eps)
+
+        numeric = np.zeros((n_components, n_features))
+        for i in range(n_components):
+            for j in range(n_features):
+                args = (YX, Gamma, Theta, epsilon_Theta, i, j)
+                x = Theta[i, j]
+                numeric[i, j] = _central_derivative(f, x, args=args)
+
+        analytic = _fembv_binx_cost_grad(YX, Gamma, Theta,
+                                         epsilon_Theta=epsilon_Theta)
+
+        self.assertTrue(np.allclose(numeric, analytic))
+
+    def test_cost_gradient_u_no_epsilon(self):
+        n_samples = 500
+        n_features = 3
+        n_components = 4
+        n_external = 4
+        n_pars = n_features * (n_external + 1)
+
+        Y = _random_binary_sequence(n_samples)
+        X = _random_affiliations(n_samples, n_features)
+        YX = np.concatenate([np.reshape(Y, (n_samples, 1)), X], axis=-1)
+
+        u = np.random.uniform(low=-10, high=10,
+                              size=(n_samples, n_external))
+
+        Theta = _random_affiliations(n_components, n_pars)
+        Gamma = _random_affiliations(n_samples, n_components)
+
+        def f(x, *args):
+            yx = args[0]
+            gamma = args[1]
+            theta = args[2].copy()
+            ext = args[3]
+            row_idx = args[4]
+            col_idx = args[5]
+
+            theta[row_idx, col_idx] = x
+
+            return _fembv_binx_cost(yx, gamma, theta, u=ext)
+
+        numeric = np.zeros((n_components, n_pars))
+        for i in range(n_components):
+            for k in range(n_external):
+                for j in range(n_features):
+                    col_idx = j + k * n_features
+                    args = (YX, Gamma, Theta, u, i, col_idx)
+                    x = Theta[i, col_idx]
+                    numeric[i, col_idx] = _central_derivative(f, x, args=args)
+
+        analytic = _fembv_binx_cost_grad(YX, Gamma, Theta, u=u)
+
+        self.assertTrue(np.allclose(numeric, analytic))
+
+    def test_cost_gradient_u_epsilon(self):
+        n_samples = 500
+        n_features = 3
+        n_components = 4
+        n_external = 4
+        n_pars = n_features * (n_external + 1)
+
+        Y = _random_binary_sequence(n_samples)
+        X = _random_affiliations(n_samples, n_features)
+        YX = np.concatenate([np.reshape(Y, (n_samples, 1)), X], axis=-1)
+
+        u = np.random.uniform(low=-10, high=10,
+                              size=(n_samples, n_external))
+        epsilon_Theta = np.random.uniform(low=2, high=10)
+
+        Theta = _random_affiliations(n_components, n_pars)
+        Gamma = _random_affiliations(n_samples, n_components)
+
+        def f(x, *args):
+            yx = args[0]
+            gamma = args[1]
+            theta = args[2].copy()
+            ext = args[3]
+            eps = args[4]
+            row_idx = args[5]
+            col_idx = args[6]
+
+            theta[row_idx, col_idx] = x
+
+            return _fembv_binx_cost(yx, gamma, theta, u=ext,
+                                    epsilon_Theta=eps)
+
+        numeric = np.zeros((n_components, n_pars))
+        for i in range(n_components):
+            for k in range(n_external):
+                for j in range(n_features):
+                    col_idx = j + k * n_features
+                    args = (YX, Gamma, Theta, u, epsilon_Theta, i, col_idx)
+                    x = Theta[i, col_idx]
+                    numeric[i, col_idx] = _central_derivative(f, x, args=args)
+
+        analytic = _fembv_binx_cost_grad(YX, Gamma, Theta,
+                                         u=u, epsilon_Theta=epsilon_Theta)
+
+        self.assertTrue(np.allclose(numeric, analytic))
