4e045ace3417c65a43c3fccdfbcaef6685adb464
Dylan Harries <Dylan.Harries@csiro.au>
2019-04-15 18:01:09 +1000

    Export FEM-BV symbols

--- pyspa/__init__.py
+++ pyspa/__init__.py
@@ -1,4 +1,15 @@
+from .fembv import (FEMBVKMeans, fembv_kmeans,
+                    FEMBVBINX, fembv_binx,
+                    PiecewiseConstantFEMBasis, TriangleFEMBasis)
 from .spa import EuclideanSPA, euclidean_spa
 
 
-__all__ = ["EuclideanSPA", "euclidean_spa"]
+__all__ = [
+    'EuclideanSPA',
+    'euclidean_spa',
+    'FEMBVKMeans',
+    'fembv_kmeans',
+    'FEMBVBINX',
+    'fembv_binx',
+    'PiecewiseConstantFEMBasis',
+    'TriangleFEMBasis']
