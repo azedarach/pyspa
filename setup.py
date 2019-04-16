a699158561d8f73c98485e58e648c833e8f5b8ee
Dylan Harries <Dylan.Harries@csiro.au>
2019-04-16 14:38:30 +1000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+MAJOR = 0
+MINOR = 0
+PATCH = 1
+VERSION = '%d.%d.%d' % (MAJOR, MINOR, PATCH)
+
+setup(name='pyspa',
+      version=VERSION,
+      description='Functions for system discretization',
+      packages=find_packages(exclude=['tests']),
+      install_requires=[
+          'joblib>=0.13.2',
+          'numpy>=1.16.2',
+          'scipy>=1.2.1',
+          'scikit-learn>=0.20.3']
+      )
