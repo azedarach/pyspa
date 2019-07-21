964c48dd992744540fdb501eae91bd37d1c54010
Dylan Harries <Dylan.Harries@csiro.au>
2019-07-22 08:35:12 +1000

    Pass random state as argument

--- pyspa/helpers/cross_validation.py
+++ pyspa/helpers/cross_validation.py
@@ -1,18 +1,24 @@
 import numpy as np
 
-def create_kfold_cv_folds(statistics_size, k):
+from sklearn.utils import check_random_state
+
+
+def create_kfold_cv_folds(statistics_size, k, random_state=None):
     if k < 2:
         raise ValueError("number of folds must be at least 2")
 
-    fold_indices = np.random.permutation(statistics_size)
+    rng = check_random_state(random_state)
+
+    fold_indices = rng.permutation(statistics_size)
 
     folds = k * [None]
     fold_size = int(np.floor(statistics_size / k))
     for i in range(k - 1):
-        folds[i] = np.sort(fold_indices[i * fold_size : (i + 1) * fold_size])
+        folds[i] = np.sort(fold_indices[i * fold_size:(i + 1) * fold_size])
     folds[k - 1] = np.sort(fold_indices[(k - 1) * fold_size:])
 
     return folds
+
 
 def get_oos_cv_masks(statistics_size, training_fraction):
     if training_fraction <= 0.0 or training_fraction >= 1.0:
@@ -24,6 +30,7 @@ def get_oos_cv_masks(statistics_size, training_fraction):
     validation_mask = ~training_mask
 
     return (training_mask, validation_mask)
+
 
 def get_kfold_cv_fold_masks(statistics_size, leave_out_indices):
     leave_in_mask = np.ones(statistics_size, dtype="bool")
